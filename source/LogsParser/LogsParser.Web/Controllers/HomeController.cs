82da30d5fbf3cb39e6659bf6c425eac2d2b8906a
oggg <ogggster@gmail.com>
2018-08-08 21:18:05 +0300

    Search form implemented

--- source/LogsParser/LogsParser.Web/Controllers/HomeController.cs
+++ source/LogsParser/LogsParser.Web/Controllers/HomeController.cs
@@ -1,30 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using LogsParser.Services.Contracts;
+using LogsParser.Web.Models;
 
 namespace LogsParser.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IDirectoryService directoryService;
+
+        public HomeController(IDirectoryService directoryService)
+        {
+            this.directoryService = directoryService;
+        }
+
         public ActionResult Index()
         {
-            return View();
-        }
+            var directories = directoryService.GetLogDirectories();
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
+            var formViewModel = new SearchFormModel()
+            {
+                Directories = directories
+            };
 
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            return View(formViewModel);
         }
     }
 }
---
+++ source/LogsParser/LogsParser.Web/Models/SearchFormModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace LogsParser.Web.Models
+{
+    public class SearchFormModel
+    {
+        public string SearchPattern { get; set; }
+
+        public IEnumerable<string> Directories { get; set; }
+    }
+}
