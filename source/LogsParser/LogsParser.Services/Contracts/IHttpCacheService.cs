b9e3fc59685e150ad715d89797f9d3e63055ba66
oggg <ogggster@gmail.com>
2018-08-08 21:56:47 +0300

    Cache service

---
+++ source/LogsParser/LogsParser.Services/Contracts/IHttpCacheService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace LogsParser.Services.Contracts
+{
+    public interface IHttpCacheService
+    {
+        T Get<T>(string itemName, Func<T> getDataFunc, int durationInSeconds);
+
+        void Remove(string itemName);
+    }
+}
