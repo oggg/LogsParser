844c2565547f5c107befd5caf9ea1ba00b07c8df
oggg <ogggster@gmail.com>
2018-08-08 23:07:30 +0300

    SignalR installation related modifications

---
+++ source/LogsParser/LogsParser.Web/Hubs/MatchHub.cs
@@ -0,0 +1,18 @@
+﻿using LogsParser.Web.Models;
+using Microsoft.AspNet.SignalR;
+
+namespace LogsParser.Web.Hubs
+{
+    public class MatchHub : Hub
+    {
+        public void FoundMatch(string folderAndFileName, int row, int col)
+        {
+            Clients.All.addLinkForMatch(new MatchModel()
+            {
+                Path = folderAndFileName,
+                Col = col,
+                Row = row
+            });
+        }
+    }
+}
---
+++ source/LogsParser/LogsParser.Web/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(LogsParser.Web.Startup))]
+
+namespace LogsParser.Web
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
