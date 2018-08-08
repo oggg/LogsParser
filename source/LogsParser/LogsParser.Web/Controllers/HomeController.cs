ff3adedf355dbbb6f2d708baf069ac20586e8e12
oggg <ogggster@gmail.com>
2018-08-08 21:57:13 +0300

    Caching directory names for 1 min

--- source/LogsParser/LogsParser.Web/Controllers/HomeController.cs
+++ source/LogsParser/LogsParser.Web/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
+using LogsParser.Common;
 using LogsParser.Services.Contracts;
 using LogsParser.Web.Models;
 
@@ -10,15 +11,19 @@ namespace LogsParser.Web.Controllers
     public class HomeController : Controller
     {
         private readonly IDirectoryService directoryService;
+        private readonly IHttpCacheService cacheService;
 
-        public HomeController(IDirectoryService directoryService)
+        public HomeController(IDirectoryService directoryService, IHttpCacheService cacheService)
         {
             this.directoryService = directoryService;
+            this.cacheService = cacheService;
         }
 
         public ActionResult Index()
         {
-            var directories = directoryService.GetLogDirectories();
+            var directories = cacheService.Get(
+                LogsParserConstants.DirectoriesCacheKey,
+                () => directoryService.GetLogDirectories(), 60);
 
             var formViewModel = new SearchFormModel()
             {
@@ -27,5 +32,7 @@ namespace LogsParser.Web.Controllers
 
             return View(formViewModel);
         }
+
+
     }
 }
