86beb568500225c60b27d3d7f08405020c73c5e0
oggg <ogggster@gmail.com>
2018-08-08 21:58:03 +0300

    SearchController

---
+++ source/LogsParser/LogsParser.Web/Controllers/SearchController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+using LogsParser.Web.Models;
+
+namespace LogsParser.Web.Controllers
+{
+    public class SearchController : Controller
+    {
+        [HttpPost]
+        public ActionResult StringMatches(SearchFormPostModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+    }
+}
