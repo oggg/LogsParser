5d2c838e23cc4308b36daa67e4c8eebc704e1375
oggg <ogggster@gmail.com>
2018-08-10 01:36:12 +0300

    Hub sends data to client on match

--- source/LogsParser/LogsParser.Web/Hubs/MatchHub.cs
+++ source/LogsParser/LogsParser.Web/Hubs/MatchHub.cs
@@ -1,18 +1,55 @@
-﻿using LogsParser.Web.Models;
+﻿using System;
+using System.IO;
+using LogsParser.Common;
+using LogsParser.Services.Contracts;
+using LogsParser.Web.Models;
 using Microsoft.AspNet.SignalR;
 
 namespace LogsParser.Web.Hubs
 {
     public class MatchHub : Hub
     {
-        public void FoundMatch(string folderAndFileName, int row, int col)
+        private readonly IDirectoryService directoryService;
+        private readonly IHttpCacheService cacheService;
+        private readonly IParseService parseService;
+
+        public MatchHub(IDirectoryService directoryService,
+            IHttpCacheService cacheService,
+            IParseService parseService)
         {
-            Clients.All.addLinkForMatch(new MatchModel()
+            this.directoryService = directoryService;
+            this.cacheService = cacheService;
+            this.parseService = parseService;
+        }
+
+        public void FindMatch(string folder, string pattern)
+        {
+            var selectedDirectory = Path.Combine(LogsParserConstants.DefaultLogsFolder, folder);
+
+            var files = cacheService.Get(
+                selectedDirectory,
+                () => directoryService.GetLogFilesPerDirectory(selectedDirectory),
+                60);
+
+            foreach (var file in files)
             {
-                Path = folderAndFileName,
-                Col = col,
-                Row = row
-            });
+                var allFileLines = File.ReadAllLines(file);
+
+                for (int i = 0; i < allFileLines.Length; i++)
+                {
+                    var row = allFileLines[i].IndexOf(pattern, StringComparison.CurrentCultureIgnoreCase);
+                    if (row >= 0)
+                    {
+                        var model = new MatchModel()
+                                        {
+                                            Path = $"{folder}/{Path.GetFileName(file)}",
+                                            Col = i,
+                                            Row = row
+                                        };
+                        Clients.All.addLinkForMatch(model);
+                    }
+                }
+            }
         }
     }
 }
