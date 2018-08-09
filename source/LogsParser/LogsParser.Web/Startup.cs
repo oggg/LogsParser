782d105690bda6cdb5a9c337628fda4e7a555e86
oggg <ogggster@gmail.com>
2018-08-09 22:56:47 +0300

    Detailed errors for signalr

--- source/LogsParser/LogsParser.Web/Startup.cs
+++ source/LogsParser/LogsParser.Web/Startup.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Owin;
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.Owin;
 using Owin;
 
 [assembly: OwinStartup(typeof(LogsParser.Web.Startup))]
@@ -9,7 +10,7 @@ namespace LogsParser.Web
     {
         public void Configuration(IAppBuilder app)
         {
-            app.MapSignalR();
+            app.MapSignalR("/signalr", new HubConfiguration() { EnableDetailedErrors = true });
         }
     }
 }
