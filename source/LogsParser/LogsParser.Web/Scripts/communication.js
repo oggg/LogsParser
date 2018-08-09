f225907df5a470ce094ce546db2120b4f70cc2ec
oggg <ogggster@gmail.com>
2018-08-10 02:04:24 +0300

    Created js files for client code

---
+++ source/LogsParser/LogsParser.Web/Scripts/communication.js
@@ -0,0 +1,24 @@
+﻿$(function () {
+    let folder = localStorage.getItem('FolderSelector');
+    let searchPattern = localStorage.getItem('SearchPattern');
+
+    if (folder && searchPattern) {
+        let matchCounter = 0;
+        let matchHubProxy = $.connection.matchHub;
+        matchHubProxy.client.addLinkForMatch = function (matchModel) {
+            matchCounter++;
+            let resultCounter = new Array(3 - matchCounter.toString().length).join(0);
+            resultCounter = resultCounter + matchCounter
+            let link = `<p>
+                                <a href="/search/getpid?path=${matchModel.Path}&row=${matchModel.Row}&col=${matchModel.Col}">
+                                    ${matchModel.Filename}_Match_${resultCounter}
+                                </a>
+                            </p>`;
+            $('#match-container').append(link);
+        }
+
+        $.connection.hub.start().done(function () {
+            matchHubProxy.server.findMatch(folder, searchPattern);
+        });
+    }
+});
---
+++ source/LogsParser/LogsParser.Web/Scripts/form.js
@@ -0,0 +1,12 @@
+﻿$(() => {
+    $('#submitButton').on('click', (event) => {
+        event.preventDefault();
+
+        $('form').serializeArray()
+            .map((v) => {
+                localStorage.setItem(v.name, v.value);
+            });
+
+        location.href = location.href + 'Search/StringMatches'
+    });
+})
